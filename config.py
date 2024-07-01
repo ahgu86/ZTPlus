e47397702559a4ba149dc460c1a95dd7e73b61fa
ahgu86 <159117127+ahgu86@users.noreply.github.com>
2024-07-01 14:09:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,19 +11,24 @@ ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
-    "上海": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/shanghai/telecom.txt"
+    "安徽": "https://chinaiptv.pages.dev/anhui/mobile.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
 search_page_num = 5
 # url关键词黑名单
 url_keywords_blacklist = []
 # 忽略的关键词，比如在demo.txt中配置广东珠江,但在订阅中只有广东珠江高清,就需要忽略掉"高清"
-search_ignore_key = ["高清", "4K"]
+search_ignore_key = ["高清","4K","HD"]
 # crawl_type的默认值为1-只爬取http://tonkiang.us上组播源；2-只爬取crawl_urls中配置的网站；3-全部
-crawl_type = "1"
+crawl_type = "3"
 # 收集其他大佬url中的直播源
 crawl_urls = [
     "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt"
+    "https://gitee.com/T-GU/kk/raw/master/teach.json",
+    "https://gitee.com/sangni2024/mingge/raw/master/1949.txt",
+    "https://m3u.ibert.me/txt/fmml_ipv6.txt",
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/ycl_iptv.txt",
 ]
 # ipv6源检测有效性的代理地址，用于不支持ipv6网络的主机，若网络支持ipv6，这里填空""
 ipv6_proxy = "http://www.ipv6proxy.net/go.php?u=" #此代理用于github，国内不一定能用
